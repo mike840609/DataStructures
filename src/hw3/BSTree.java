1b842b944cbaf86761143c58a5fbe46da99784b3
mike840609 <mike840609@gmail.com>
2016-01-14 16:23:40 +0800

    node search finish

--- src/hw3/BSTree.java
+++ src/hw3/BSTree.java
@@ -4,33 +4,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.io.*;
 
-class TreeNode
-{ // 樹節點類別
+class TreeNode { // 樹節點類別
 	int data; // 節點資料
 	TreeNode left; // 參考左子樹
 	TreeNode right; // 參考右子樹
 
 	// 建構子
-	public TreeNode(int data)
-	{
+	public TreeNode(int data) {
 		this.data = data;
 		this.left = this.right = null;
 	}
 }
 
-public class BSTree
-{ // 二元搜尋樹類別
+public class BSTree { // 二元搜尋樹類別
 	public TreeNode head; // 參考樹的根節點
 	// 建立空的二元搜尋樹
 
-	public BSTree()
-	{
+	public BSTree() {
 		head = null;
 	}
 
 	// 方法: 檢查二元搜尋樹是否是空的
-	boolean isBSTreeEmpty()
-	{
+	boolean isBSTreeEmpty() {
 		if (head == null)
 			return true;
 		else
@@ -38,86 +33,64 @@ public class BSTree
 	}
 
 	// 方法: 在二元搜尋樹插入節點
-	public void insertBSTreeNode(int data)
-	{
-		if (head == null)
-		{ // 建立根節點
+	public void insertBSTreeNode(int data) {
+		if (head == null) { // 建立根節點
 			head = new TreeNode(data);
 			return;
 		}
 		// 建立二元搜尋樹
 		TreeNode current = head;
-		while (true)
-		{
+		while (true) {
 			// 在左子樹
-			if (data < current.data)
-			{
+			if (data < current.data) {
 				// 左子樹為空
-				if (current.left == null)
-				{
+				if (current.left == null) {
 					current.left = new TreeNode(data);
 					return;
-				}
-				else
+				} else
 					current = current.left; // 若左子樹有值則把current指標移植左節點在繼續往下找
 
-			}
-			else
-			{ // 在右子樹
-				if (current.right == null)
-				{
+			} else { // 在右子樹
+				if (current.right == null) {
 					current.right = new TreeNode(data);
 					return;
-				}
-				else
+				} else
 					current = current.right;
 			}
 		}
 	}
 
 	// 方法: 顯示二元搜尋樹的節點資料
-	public void printBSTree()
-	{
+	public void printBSTree() {
 		inOrder(this.head);
 		System.out.println();
 	}
 
 	// 方法 刪除節點 未完成 未用到
 	// 改用陣列方法完成節點刪除
-	public void deleteNode(int target)
-	{
+	public void deleteNode(int target) {
 		TreeNode del_node;
 		del_node = head;
 
 		// 跟樹根做比較 大則往又跑 小則往左找
-		while (true)
-		{
+		while (true) {
 			// 在左子樹
-			if (target < del_node.data)
-			{
+			if (target < del_node.data) {
 				// 左子樹為空
-				if (del_node.left.data == target)
-				{
+				if (del_node.left.data == target) {
 					del_node.left = null;
 					return;
-				}
-				else if (del_node.right.data == target)
-				{
+				} else if (del_node.right.data == target) {
 					del_node.right = null;
 					return;
-				}
-				else
+				} else
 					del_node = del_node.left; // 若左子樹有值則把current指標移植左節點在繼續往下找
 
-			}
-			else
-			{ // 在右子樹
-				if (del_node.right == null)
-				{
+			} else { // 在右子樹
+				if (del_node.right == null) {
 					del_node.right = new TreeNode(target);
 					return;
-				}
-				else
+				} else
 					del_node = del_node.right;
 			}
 		}
@@ -125,46 +98,35 @@ public class BSTree
 	}
 
 	// 方法 階層搜尋
-	public void layerSearch(int data, TreeNode head)
-	{
+	public void layerSearch(int data, TreeNode head) {
 		TreeNode current = head;
 		int layer = 0;
 
-		if (data < current.data)
-		{
-			if (current.left.data == data)
-			{
+		while (current != null) {
+			// if (current != null || current.right != null) {
+			if (data < current.data&&(current.left!=null||current.right!=null)) {
 				layer++;
-				System.out.println("位於階層:" + layer);
-			}
-			else if (current.data != data || current.left == null)
-			{
-				System.out.println("找不到此節點");
-			}
-			else
-			{
 				current = current.left;
+				
+			} else if (data > current.data&&(current.left!=null||current.right!=null)) {
 				layer++;
-				// 若左子樹有值則把current指標移植左節點在繼續往下找
-			}
-		}
-		else if (data > current.data)
-		{ // 在右子樹
-			if (current.right.data == data)
-			{
-				layer++;
-				System.out.println("位於階層:" + layer);
-			}
-			else
 				current = current.right;
+
+				// }
+			} else if (data == current.data) {
+				System.out.println("節點位於第:" + layer + "層");
+				break;
+			} else {
+				System.out.println("找不節點");
+				break;
+			}
 		}
+		current = head;
 	}
 
 	// 方法1: 前序走訪的遞迴方法
-	public void preOrder(TreeNode node)
-	{
-		if (node != null)
-		{
+	public void preOrder(TreeNode node) {
+		if (node != null) {
 			// 顯示節點資料
 			System.out.print("[" + node.data + "]");
 			preOrder(node.left); // 走訪左子樹
@@ -173,10 +135,8 @@ public class BSTree
 	}
 
 	// 方法2: 中序走訪的遞迴方法
-	public void inOrder(TreeNode node)
-	{
-		if (node != null)
-		{
+	public void inOrder(TreeNode node) {
+		if (node != null) {
 			inOrder(node.left); // 走訪左子樹
 			// 顯示節點資料
 			System.out.print("[" + node.data + "]");
@@ -185,10 +145,8 @@ public class BSTree
 	}
 
 	// 方法3: 後序走訪的遞迴方法
-	public void postOrder(TreeNode node)
-	{
-		if (node != null)
-		{
+	public void postOrder(TreeNode node) {
+		if (node != null) {
 			postOrder(node.left); // 走訪左子樹
 			postOrder(node.right); // 走訪右子樹
 			// 顯示節點資料
@@ -196,81 +154,57 @@ public class BSTree
 		}
 	}
 
-	public void writerTxt(ArrayList<Integer> array)
-	{
+	public void writerTxt(ArrayList<Integer> array) {
 		BufferedWriter fw = null;
 		String filePath = hw3.class.getResource("").getPath().replace("file:", "") + "/writeTest.txt"; // 文件和該類在同個目錄下
-		try
-		{
+		try {
 			File file = new File(filePath);
 			fw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8")); // 指點編碼格式，以免讀取時中文字符異常
 			// new FileOutputStream(file,true)添加 new FileOutputStream(file)覆蓋
 			// fw.append("23,57,8,45,12,33,56,6,77,37");
-			for (int i = 0; i < array.size(); i++)
-			{
+			for (int i = 0; i < array.size(); i++) {
 				fw.append(String.valueOf(array.get(i)) + ",");
 			}
 			fw.flush(); // 全部寫入緩存中的內容
 			System.out.println("binarytree.txt was created");
 
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally
-		{
-			if (fw != null)
-			{
-				try
-				{
+		} finally {
+			if (fw != null) {
+				try {
 					fw.close();
-				}
-				catch (IOException e)
-				{
+				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
 		}
 	}
 
-	public void readTxt()
-	{
+	public void readTxt() {
 		String filePath = hw3.class.getResource("").getPath().replace("file:", "") + "/text.txt"; // 文件和該類在同個目錄下
 		BufferedReader reader = null;
-		try
-		{
+		try {
 			reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), "UTF-8")); // 指定讀取文件的編碼格式，以免出現中文亂碼
 			String str = null;
-			while ((str = reader.readLine()) != null)
-			{
+			while ((str = reader.readLine()) != null) {
 				System.out.println(str);
 			}
-		}
-		catch (FileNotFoundException e)
-		{
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		finally
-		{
-			try
-			{
+		} finally {
+			try {
 				reader.close();
-			}
-			catch (IOException e)
-			{
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 
 	}
 
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		java.util.Scanner sc = new java.util.Scanner(System.in);
 		int i;
 
@@ -298,8 +232,7 @@ public class BSTree
 		// }
 
 		// 改用arrayListc後 迴圈 寫法
-		for (i = 0; i < array.size(); i++)
-		{
+		for (i = 0; i < array.size(); i++) {
 			System.out.print("[" + array.get(i) + "]");
 			obj.insertBSTreeNode(array.get(i));
 		}
@@ -322,15 +255,13 @@ public class BSTree
 		// I = insert node
 		// d = delete node
 		// s = search node
-		while (true)
-		{
+		while (true) {
 			System.out.println("輸入動作");
 			System.out.println(
 					" i=>insert node\n q=>Inorder Traversal\n s=>layer search\n d=>delete node\n anykey=> print all node");
 			String option = sc.next();
 
-			if (option.equals("i"))
-			{
+			if (option.equals("i")) {
 				System.out.println("輸入值");
 				int value = sc.nextInt();
 				obj.insertBSTreeNode(value);
@@ -338,49 +269,40 @@ public class BSTree
 				obj.writerTxt(array);
 			}
 
-			else if (option.equals("q"))
-			{
+			else if (option.equals("q")) {
 				System.out.println("Inorder Traversal 走訪");
 				obj.inOrder(obj.head);
 				System.out.println("");
 			}
 
-			else if (option.equals("s"))
-			{
+			else if (option.equals("s")) {
 				System.out.println("節點階層搜尋");
 				System.out.println("輸入搜尋節點：");
 				int value = sc.nextInt();
 				obj.layerSearch(value, obj.head);
 			}
 
-			else if (option.equals("d"))
-			{
+			else if (option.equals("d")) {
 				System.out.println("節點刪除");
 				System.out.println("輸入刪除節點：");
 				int value = sc.nextInt();
 				// obj.deleteNode(value);
-				if (array.contains(value))
-				{
+				if (array.contains(value)) {
 					array.remove(array.indexOf(value));
 					ArrayList<Integer> temp = array;
 					// 將樹清空
 					obj = new BSTree();
-					for (i = 0; i < temp.size(); i++)
-					{
+					for (i = 0; i < temp.size(); i++) {
 						System.out.print("[" + temp.get(i) + "]");
 						obj.insertBSTreeNode(temp.get(i));
 					}
-				}
-				else
-				{
+				} else {
 					System.out.println(value + " Not found");
 				}
 			}
 
-			else
-			{
-				for (i = 0; i < array.size(); i++)
-				{
+			else {
+				for (i = 0; i < array.size(); i++) {
 					System.out.print("[" + array.get(i) + "]");
 				}
 				System.out.println("");
